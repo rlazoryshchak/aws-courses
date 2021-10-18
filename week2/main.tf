2c25b1eaf392c92c106ca1615d495c2fe7b833df
Roman Lazoryshchak <rlazoryshchak@lohika.com>
2021-10-18 21:18:12 +0300

    Add role creation to tf

--- week2/main.tf
+++ week2/main.tf
@@ -84,5 +84,44 @@ resource "aws_security_group" "http" {
 
 resource "aws_iam_instance_profile" "test_profile" {
   name = "test_profile"
-  role = "s3-full-access"
+  role = aws_iam_role.test_role.name
+}
+
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:s3:::rlazoryshchak-bucket", 
+          "arn:aws:s3:::rlazoryshchak-bucket/*"
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
